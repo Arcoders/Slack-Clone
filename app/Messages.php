3a57420c134151238ebec505c04575b90f759ff2
arcoders <anonismaa@gmail.com>
2017-11-24 03:15:55 +0100

    create models and tables

---
+++ app/Messages.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Messages extends Model
+{
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function room()
+    {
+        return $this->belongsTo(Rooms::class);
+    }
+
+}
