c5d4a579008bca54927d090312cce4436a31b8a1
arcoders <anonismaa@gmail.com>
2017-11-24 01:37:50 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Welcome</div>
+
+                    <div class="panel-body">
+                        Landing Page
+                    </div>
+                </div>
+            </div>
+@endsection
